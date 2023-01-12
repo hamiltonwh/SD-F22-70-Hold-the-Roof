0f6a357f82527e08ec3f634cb3d8437a44a0f05b
Ramon <ramonchavez@mines.edu>
2023-01-12 09:32:38 -0700

    Opening a camera

--- CV/ITT_2/main.py
+++ CV/ITT_2/main.py
@@ -1,1 +1,22 @@
-print("Hi")
+import cv2 as cv
+
+
+def main():
+    camport1 = 1
+    # camport2 = 2
+
+    cam1 = cv.VideoCapture(camport1)
+    _, img1 = cam1.read()
+    width, height = img1.shape[1], img1.shape[0]
+
+
+    while True:
+        _, img1 = cam1.read()
+        print(width, height)
+        cv.imshow("Image 1",img1)
+        cv.waitKey(0)
+
+
+
+if __name__ == "__main__":
+    main()
