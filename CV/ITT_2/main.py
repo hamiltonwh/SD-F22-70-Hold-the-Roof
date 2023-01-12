81c1bb928e54c3d1a3bccf334047345e4c35269e
Ramon <ramonchavez@mines.edu>
2023-01-12 10:15:32 -0700

    Adding camera values

--- CV/ITT_2/main.py
+++ CV/ITT_2/main.py
@@ -13,11 +13,20 @@ def main():
     _, img2 = cam_left.read()
     width, height = img1.shape[1], img1.shape[0]
 
+    # Camera values
+    UndistMatrix = np.array([[694.17008191, 0., 304.53123739],
+                                 [0., 698.24565396, 195.44512563],
+                                 [0., 0., 1.]])
+    distCoeff = np.array([[-3.33453589e-01, -8.39226365e-02, 4.31567352e-03, 4.77064083e-04,
+                               5.71389403e-01]])
 
     while True:
         _, img1 = cam_right.read()
-        print(width, height)
-        cv.imshow("Image 1",img1)
+
+        img1_und = cv.undistort(img1,cameraMatrix=UndistMatrix, distCoeffs=distCoeff)
+        # print(width, height)
+
+        cv.imshow("Image 1",np.hstack([img1,img1_und]))
         cv.waitKey(0)
 
 
